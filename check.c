af3af00e9a22cc174e9422ca0b6f2b27507a389d
rscres <rseelaen@student.42sp.org.br>
2023-09-18 17:55:11 -0300

    add int check function to check.c

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:51:36 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/09/18 15:23:37 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/09/18 17:49:25 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,34 @@ int	check_doubles(t_dbl_list *stack_a)
 		while (tmp)
 		{
 			if (*((int *)tmp->content) == *((int *)stack_a->content))
-				return (0);
+				return (1);
 			tmp = tmp->next;
 		}
 		stack_a = stack_a->next;
+	}
+	return (0);
+}
+
+int	check_int(char **arg)
+{
+	int			i;
+	int			j;
+	long int	num;
+
+	i = 0;
+	while (arg[i])
+	{
+		j = 0;
+		num = ft_atol(arg[i]);
+		if (num > INT_MAX || num < INT_MIN)
+			return (0);
+		while (arg[i][j])
+		{
+			if (!ft_isdigit(arg[i][j]) && arg[i][j] != '-')
+				return (0);
+			j++;
+		}
+		i++;
 	}
 	return (1);
 }
