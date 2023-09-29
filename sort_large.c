1ac94a0a7151e4863e34d43db6c8b5c7f911a8a9
rscres <rseelaen@student.42sp.org.br>
2023-09-29 18:24:06 -0300

    Daily commit: start modifying logic of sort_large, start sort by middle numbers. Working about as well as the othher logic

--- sort_large.c
+++ sort_large.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 16:02:14 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/09/28 18:28:44 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/09/29 18:15:38 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ int	piece_size(t_dbl_list **stack)
 		return (size / 21);
 }
 
-int	find_cheap_high(t_dbl_list *stack, int piece)
+int	find_cheap_high(t_dbl_list *stack, int start)
 {
 	int	mid;
 	int	i;
@@ -68,15 +68,16 @@ int	find_cheap_high(t_dbl_list *stack, int piece)
 	i = 0;
 	while (stack && i < mid)
 	{
-		if (stack->index < piece)
+		if (stack->index >= start && stack->index <= mid)
 			break ;
 		stack = stack->next;
 		i++;
 	}
+	// printf("high index= %d\n", stack->index);
 	return (i);
 }
 
-int	find_cheap_low(t_dbl_list *stack, int piece)
+int	find_cheap_low(t_dbl_list *stack, int end)
 {
 	int			mid;
 	int			i;
@@ -89,11 +90,12 @@ int	find_cheap_low(t_dbl_list *stack, int piece)
 	i = 1;
 	while (stack && i <= mid)
 	{
-		if (tmp->index < piece)
+		if (tmp->index <= end && tmp->index > mid)
 			break ;
 		tmp = tmp->prev;
 		i++;
 	}
+	// printf("low index= %d\n", tmp->index);
 	return (i);
 }
 
@@ -140,6 +142,56 @@ int	find_next_larger(t_dbl_list *stack, int index)
 	return (next);
 }
 
+int	set_start(int size_piece, t_dbl_list *stack_a)
+{
+	static int	times;
+	static int	mid;
+	int			start;
+
+	if (times == 0)
+		times = 1;
+	if (!mid)
+		mid = ft_dbl_lstsize(stack_a) / 2;
+	start = mid - (size_piece * times);
+	if (start < 0)
+		start = 0;
+	times++;
+	return (start);
+}
+
+int	set_end(int size_piece, t_dbl_list *stack_a)
+{
+	static int	times;
+	static int	mid;
+	static int	size;
+	int			end;
+
+	if (times == 0)
+		times = 1;
+	if (!size)
+		size = ft_dbl_lstsize(stack_a);
+	if (!mid)
+		mid = size / 2;
+	end = mid + (size_piece * times);
+	if (end > size)
+		end = size;
+	times++;
+	return (end);
+}
+
+int	search_piece(t_dbl_list *stack, int start, int end)
+{
+	while (stack)
+	{
+		if (stack->index >= start && stack->index <= end)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
+}
+
+
+
 void	algo_large(t_dbl_list **stack_a)
 {
 	t_dbl_list	**stack_b;
@@ -148,35 +200,63 @@ void	algo_large(t_dbl_list **stack_a)
 	int			cheap_high;
 	int			cheap_low;
 	int			counter;
+	int			start;
+	int			end;
+	int mid = ft_dbl_lstsize(*stack_a) / 2;
+	int			start_counter;
+	int			end_counter;
 
 	stack_b = malloc(sizeof(t_dbl_list *));
 	*stack_b = NULL;
 	size_piece = piece_size(stack_a);
 	counter = 0;
 	cur_piece = size_piece;
+	start = set_start(size_piece, *stack_a);
+	end = set_end(size_piece, *stack_a);
+	start_counter = 0;
+	end_counter = 0;
 	while (*stack_a)
 	{
-		if (counter == cur_piece - 1)
-			cur_piece += size_piece;
-		cheap_high = find_cheap_high(*stack_a, cur_piece);
-		cheap_low = find_cheap_low(*stack_a, cur_piece);
-		if (cheap_high <= cheap_low)
+		if (start_counter == start)
 		{
-			while (cheap_high--)
-				rotate_a(stack_a);
-			push_b(stack_a, stack_b);
+			start = set_start(size_piece, *stack_a);
+			start_counter = 0;
 		}
-		else
+		if (end_counter == end)
 		{
+			end = set_end(size_piece, *stack_a);
+			end_counter = 0;
+		}
+		cheap_high = find_cheap_high(*stack_a, start);
+		cheap_low = find_cheap_low(*stack_a, end);
+		// printf("cheap_high = %d\n", cheap_high);
+		// printf("cheap_low = %d\n", cheap_low);
+		if (cheap_high > cheap_low)
+		{
+			if (cheap_low < mid)
+				start_counter++;
+			else
+				end_counter++;
 			while (cheap_low--)
 				reverse_rotate_a(stack_a);
 			push_b(stack_a, stack_b);
+			if ((*stack_b)->index < mid)
+				rotate_b(stack_b);
 		}
-		if (ft_dbl_lstsize(*stack_a) <= 3)
-			while (!check_sorted(*stack_a))
-				algo_3arg(stack_a);
-		if (*stack_b && (*stack_b)->next && (*stack_b)->index < (*stack_b)->next->index)
-			swap_b(stack_b);
+		else
+		{
+			if (cheap_low < mid)
+				start_counter++;
+			else
+				end_counter++;
+			// printf("cheap_high > cheap_low\n");
+			while (cheap_high--)
+				rotate_a(stack_a);
+			push_b(stack_a, stack_b);
+			if ((*stack_b)->index < mid)
+				rotate_b(stack_b);
+
+		}
 		if (check_sorted(*stack_a))
 			break ;
 		counter++;
