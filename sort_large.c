16763b0b96f8f98634e8ca032bf17e28f62e44d9
rscres <rseelaen@student.42sp.org.br>
2023-10-06 20:15:58 -0300

    create move_to_a function to split sort_large function; still have issues on worst case arrays

--- sort_large.c
+++ sort_large.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 16:02:14 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/10/05 20:42:36 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/10/06 19:28:59 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -190,7 +190,7 @@ void	set_start_end(t_data *data, t_dbl_list **stack_a)
 	data->end_counter = 0;
 }
 
-void	move_to_a(t_dbl_list **stack_a, t_dbl_list **stack_b, int mid)
+void	move_to_b(t_dbl_list **stack_a, t_dbl_list **stack_b, int mid)
 {
 	t_data	data;
 
@@ -259,34 +259,37 @@ int	check_b_cheaper(t_dbl_list *stack_a, t_dbl_list *stack_b)
 	int	moves_a;
 	int	moves_b;
 	int	target;
+	static int	piece;
 
+	// if (!piece)
+	// 	piece = piece_size(stack_b);
 	target = find_next_larger(stack_a, stack_b->index);
 	if (target == -1)
-		target = find_min_index(stack_a);
+	{	
+		if (find_min_index(stack_a) <= find_max_index(stack_a))
+			target = find_min_index(stack_a);
+		else
+			target = find_max_index(stack_a);
+	}
+	// if (target == -1)
+	// 	target = find_min_index(stack_a);
 	moves_a = find_place_index(stack_a, target);
 	if (moves_a > ft_dbl_lstsize(stack_a) / 2)
-		moves_a = ft_dbl_lstsize(stack_a) - moves_a;
+		moves_a = (ft_dbl_lstsize(stack_a) - moves_a) + 1;
 	target = find_max_index(stack_b);
 	moves_b = find_place_index(stack_b, target);
 	if (moves_b > ft_dbl_lstsize(stack_b) / 2)
-		moves_b = ft_dbl_lstsize(stack_b) - moves_b;
+		moves_b = (ft_dbl_lstsize(stack_b) - moves_b) + 1;
 	if (moves_a == -1 || moves_a < moves_b)
 		return (0);
 	return (1);
 }
 
-void	sort_large(t_dbl_list **stack_a)
+void	move_to_a(t_dbl_list **stack_a, t_dbl_list **stack_b, int mid)
 {
-	t_dbl_list	**stack_b;
-	int			mid;
-	int			target;
-	int			place;
+	int	target;
+	int	place;
 
-	stack_b = malloc(sizeof(t_dbl_list *));
-	*stack_b = NULL;
-	mid = ft_dbl_lstsize(*stack_a) / 2;
-	move_to_a(stack_a, stack_b, mid);
-	sort_3elem(stack_a);
 	while (*stack_b)
 	{
 		if (check_b_cheaper(*stack_a, *stack_b))
@@ -305,19 +308,30 @@ void	sort_large(t_dbl_list **stack_a)
 					reverse_rotate_a(stack_a);
 			push_a(stack_b, stack_a);
 		}
-		else if (place <= mid)
+		else
 		{
-			while ((*stack_a)->index != target)
-				rotate_a(stack_a);
-			push_a(stack_b, stack_a);
-		}
-		else if (place > mid)
-		{
-			while ((*stack_a)->index != target)
-				reverse_rotate_a(stack_a);
+			if (place <= mid)
+				while ((*stack_a)->index != target)
+					rotate_a(stack_a);
+			else
+				while ((*stack_a)->index != target)
+					reverse_rotate_a(stack_a);
 			push_a(stack_b, stack_a);
 		}
 	}
+}
+
+void	sort_large(t_dbl_list **stack_a)
+{
+	t_dbl_list	**stack_b;
+	int			mid;
+
+	stack_b = malloc(sizeof(t_dbl_list *));
+	*stack_b = NULL;
+	mid = ft_dbl_lstsize(*stack_a) / 2;
+	move_to_b(stack_a, stack_b, mid);
+	sort_3elem(stack_a);
+	move_to_a(stack_a, stack_b, mid);
 	if (!check_sorted(*stack_a))
 	{
 		mid = ft_dbl_lstsize(*stack_a) / 2;
